66e34247c856f412307c6544187ddbe51e0d8359
Bruce <bruce@mackinnon.com>
2023-01-16 11:03:09 -0500

    Major revision (NOT TESTED ON CAR)

--- hello-obd2.ino
+++ hello-obd2.ino
@@ -1,165 +1,200 @@
 /*
+Very simple OBD2 scanner for ISO 9141-2 vehicles
+Bruce MacKinnon 16-Jan-2023
+
+Example RPM Query
 Sent:
-68
-6a
-f1
-1
-c
-d0
-
+68 6a f1 1 c d0
 Recd:
-48
-6b
-10
-41
-c
-18
-f
-37
- */
-
-
+48 6b 10 41 c 18 f 37
+*/
 const byte rxPin = 0;
 const byte txPin = 1;
 const byte LED_PIN = 13;
-
-int zeroState = LOW;
-int oneState = HIGH;
-
-void generateInit() {
-
-  // Start bin
-  digitalWrite(txPin, zeroState);
-  delay(200);
-  // Send 00110011
-  digitalWrite(txPin, oneState);
-  delay(200);
-  delay(200);
-  digitalWrite(txPin, zeroState);
-  delay(200);
-  delay(200);
-  digitalWrite(txPin, oneState);
-  delay(200);
-  delay(200);
-  digitalWrite(txPin, zeroState);
-  delay(200);
-  delay(200);
-  // Stop bit (idle state)
-  digitalWrite(txPin, oneState);
-  delay(200);
-}
-
-// inspired by SternOBDII\code\checksum.c
-static byte iso_checksum(byte *data, byte len)
-{
-  byte crc=0;
-  for(byte i=0; i<len; i++)
-    crc=crc+data[i];
-  return crc;
-}
-
+const long RX_TIMEOUT_MS = 100;
 const long W1 = 20;
 const long W4 = 30;
 
 int state = 0;
 long stamp0 = 0;
-long stamp3 = 0;
+long lastActivityStamp = 0;
 byte kw0 = 0;
 byte kw1 = 0;
 
-void setup() {
-
-    Serial.begin(9600);
-    // Define pin modes for TX and RX
-    pinMode(rxPin, INPUT);
-    pinMode(txPin, OUTPUT);
-    // Idle state for TX
-    digitalWrite(txPin, HIGH);
-    pinMode(LED_PIN, OUTPUT);
-    // Idle state for the LED pin
-    digitalWrite(LED_PIN, LOW);
-
-    // Stobe LED
-    digitalWrite(LED_PIN, HIGH);
-    delay(500);
-    digitalWrite(LED_PIN, LOW);
-    delay(500);
-    digitalWrite(LED_PIN, HIGH);
-    delay(500);
-    digitalWrite(LED_PIN, LOW);
-    delay(500);
-
-    Serial.println("OBD2 Diagnostic Scanner");
-   
-    delay(2000);
-
-    // Send the 5-baud init
-    Serial.println("Connecting to ECU ...");
-
-    generateInit();
-    
-    // Set the baud rate for the serial port
-    Serial1.begin(10400);
-        
-    state = 0;
-    stamp0 = millis();    
-}
-
-int diagState = 0;
+// Number of received byte to ignore - used to eliminate
+// process of bytes that we send on the K-Line (i.e. echo
+// ignore).
 int ignoreCount = 0;
 
-byte rxMsg[16];
+// This is the buffer that we use to accumulate received data
+// from the vehicle.
+const int MAX_RX_MSG_LEN = 256;
+byte rxMsg[MAX_RX_MSG_LEN];
 int rxMsgLen = 0;
+
+int cycle = 0;
+
+/**
+ * Sends the ISO9141-2 compliant 5-baud initilization 
+ * sequence.  This is a hex 33.
+ */
+static void generateFiveBaudInit() {
+  // Let things idle long enough to time out the ECU
+  delay(5500);
+  // Start bin
+  digitalWrite(txPin, LOW);
+  delay(200);
+  // Send 00110011
+  digitalWrite(txPin, HIGH);
+  delay(200);
+  delay(200);
+  digitalWrite(txPin, LOW);
+  delay(200);
+  delay(200);
+  digitalWrite(txPin, HIGH);
+  delay(200);
+  delay(200);
+  digitalWrite(txPin, LOW);
+  delay(200);
+  delay(200);
+  // Stop bit (idle state)
+  digitalWrite(txPin, HIGH);
+  delay(200);
+}
+
+// Inspired by SternOBDII\code\checksum.c
+static byte iso_checksum(byte *data, byte len) {
+  byte crc = 0;
+  for (byte i = 0; i < len; i++)
+    crc = crc + data[i];
+  return crc;
+}
+
+void setup() {
+
+  // Console
+  Serial.begin(9600);
+
+  // Define pin modes for TX and RX
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+  // Idle state for TX
+  digitalWrite(txPin, HIGH);
+  pinMode(LED_PIN, OUTPUT);
+  // Idle state for the LED pin
+  digitalWrite(LED_PIN, LOW);
+
+  // Stobe LED so that we know things are running
+  digitalWrite(LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_PIN, LOW);
+  delay(500);
+  digitalWrite(LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_PIN, LOW);
+  delay(500);
+
+  Serial.println("OBD2 Diagnostic Scanner");
+ 
+  delay(2000);
+
+  // Send the 5-baud init
+  Serial.println("Connecting to ECU ...");
+  
+  generateFiveBaudInit();
+    
+  // Set the baud rate for the ISO9141 serial port
+  Serial1.begin(10400);
+        
+  state = 0;
+  stamp0 = millis();    
+  lastActivityStamp = millis();
+}
 
 void loop() {
 
   long now = millis();
 
+  // Waiting for the 0x55
+  if (state == 0) {
+    if ((now - lastActivityStamp) > 1000) {
+      Serial.println("ERROR 0: Timed out waiting for 0x55");
+      state = 99;  
+    }
+  }
+  
   // Waiting to send initialization ACK
-  if (state == 3) {
+  else if (state == 3) {
       // Wait W4
-      if (now - stamp3 < W4) {
+      if (now - lastActivityStamp < W4) {
+        // PAUSE AFTER RECEIVING THE KW1
       } 
       else {
-        state = 5;
         // Send ACK
         Serial1.write(~kw1);        
         ignoreCount++;
+        state = 5;
+        lastActivityStamp = now;
       }
   }
 
-  // Waiting to send command
+  // Waiting to send a command to the vehicle
   else if (state == 6) {
-    // 500 didn't work
-    delay(1000);
-    // RPM query
-    byte msg[6] = { 0x68, 0x6a, 0xf1, 0x1, 0x0c, 0x0 };
-    msg[5] = iso_checksum(msg, 5);
-    // 00
-    //byte msg[6] = { 0x68, 0x6a, 0xf1, 0x1, 0x00, 0xc4 };
-    ignoreCount = 6;
-    Serial1.write(msg, 6);
-    state = 7;
-    Serial.println("Sent query");
+
+    // We only generate a command during quiet periods
+    if ((now - lastActivityStamp) < 1000) {
+      // PAUSE
+    }
+    else {
+      if (cycle == 0) {
+        // RPM 
+        byte msg[6] = { 0x68, 0x6a, 0xf1, 0x1, 0x0c, 0x0 };
+        msg[5] = iso_checksum(msg, 5);
+        ignoreCount = 6;
+        Serial1.write(msg, 6);
+      } else if (cycle == 1) {
+        // Fuel pressure
+        byte msg[6] = { 0x68, 0x6a, 0xf1, 0x1, 0x0a, 0x0 };
+        msg[5] = iso_checksum(msg, 5);
+        ignoreCount = 6;
+        Serial1.write(msg, 6);
+      } else if (cycle == 2) {
+        // Manifold pressure
+        byte msg[6] = { 0x68, 0x6a, 0xf1, 0x1, 0x0b, 0x0 };
+        msg[5] = iso_checksum(msg, 5);
+        ignoreCount = 6;
+        Serial1.write(msg, 6);
+      }
+
+      // Wrap around
+      cycle++;
+      if (cycle == 3) {
+        cycle = 0;
+      }
+      
+      lastActivityStamp = now;
+    }
   }
 
+  // Check to see if we have any data from the K-Line
   if (Serial1.available() > 0) {
 
-    // Read a byte from the vehicle
+    // Read a byte from the K-Line
     int r = Serial1.read();
 
+    /*
     // TEMP
     {
       char buf[16];
       sprintf(buf,"%x",(int)r);
       Serial.println(buf);
     }
+    */
     
     // The K-Line has transmit and receive data so check to see 
-    // if we should ignore our own transmission
+    // if we should ignore our own transmission.
     if (ignoreCount > 0) {
       ignoreCount--;
-      //Serial.println("(Ignored)");
     } 
     else {
       
@@ -167,7 +202,7 @@ void loop() {
       if (state == 0) {
         // Throw away garbage during W1
         if (now - stamp0 < W1) {
-          Serial.println("(Garbage ignored)");
+          // DO NOTHING IN THIS WINDOW
         } 
         else {
           if (r == 0x55) {
@@ -175,10 +210,7 @@ void loop() {
           }
           else {
             state = 99;
-            Serial.println("ERROR 0: Bad 0x55 ACK");
-            char buf[16];
-            sprintf(buf,"%x",(int)r);
-            Serial.println(buf);
+            Serial.println("ERROR 1: Bad 0x55 ACK");
           }
         }
       }
@@ -191,13 +223,12 @@ void loop() {
       else if (state == 2) {
         kw1 = (byte)r;
         state = 3;     
-        stamp3 = millis();
       }
       // Waiting for ack
       else if (state == 5) {
         // This is the inverse of 0x33
         if (r == 0xcc) {
-            Serial.println("ECU connection was successful!");
+            Serial.println("INFO: ECU connection was successful!");
             state = 6;
         } else {
             Serial.println("ERROR 5: Bad 0xcc ACK");
@@ -205,29 +236,64 @@ void loop() {
         }
       }
   
-      // Generic display
-      else if (state == 7) {
+      // Generic data accumulation state.  When a full message 
+      // is received we process and reset the accumulator.
+      else if (state == 6) {
+
         /*
         // DEBUG DISPLAY
         char buf[16];
         sprintf(buf,"%x ",(int)r);
         Serial.print(buf);
         */
-        // Accumulate
+        
+        // If we've had a significant pause since the 
+        // laste byte then reset the accumulation counter
+        // and assume this was a partial message fragment.
+        if (rxMsgLen > 0 && (now - lastActivityStamp) > RX_TIMEOUT_MS) {
+          rxMsgLen = 0;
+          Serial.println("WARN: Discarded partial message");
+        }
+
+        // Accumulate and wrap if necessary
         rxMsg[rxMsgLen++] = (byte)r;
+        rxMsgLen = rxMsgLen % MAX_RX_MSG_LEN;
 
         // Look for a complete message
-        if (rxMsgLen == 8) {
-          unsigned int rpm = ((rxMsg[5] * 256) + rxMsg[6]) / 4;
-          rxMsgLen = 0;
-          char buf[16];
-          sprintf(buf,"RPM %d",(int)rpm);
-          Serial.println();
-          Serial.println(buf);
-          // Switch back for a new query
-          state = 6;
+        if (rxMsg[0] == 0x48 && rxMsg[1] == 0x6b) {
+          // RPM Message?
+          if (rxMsgLen == 8 && rxMsg[4] == 0x0c) {
+            unsigned int rpm = ((rxMsg[5] * 256) + rxMsg[6]) / 4;
+            char buf[64];
+            sprintf(buf,"DATA: Tach: %d rpm",(int)rpm);
+            Serial.println(buf);
+            // Reset the accumulator
+            rxMsgLen = 0;
+          }
+          // Fuel Pressure?
+          else if (rxMsgLen == 7 && rxMsg[4] == 0x0a) {
+            unsigned int p = rxMsg[5] * 3;
+            char buf[64];
+            sprintf(buf,"DATA: Fuel Pressure: %d kPa",(int)p);
+            Serial.println(buf);
+            // Reset the accumulator
+            rxMsgLen = 0;
+          }
+          // Intake Manifold Pressure?
+          else if (rxMsgLen == 7 && rxMsg[4] == 0x0b) {
+            unsigned int p = rxMsg[5];
+            char buf[64];
+            sprintf(buf,"DATA: Intake Manifold Pressure: %d kPa",(int)p);
+            Serial.println(buf);
+            // Reset the accumulator
+            rxMsgLen = 0;
+          }
         }
       }
     }
+    
+    // Keep track of the last time we got activity on the 
+    // K-Line so that we can detect quiet times.
+    lastActivityStamp = now;
   }
 }
